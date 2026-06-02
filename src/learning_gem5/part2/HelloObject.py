e13e8b8ef73d608c621fae9b78e0b061c1a4b4f1
aaaaa-ozo23 <xuhanyu89@gmail.com>
2026-06-02 14:06:13 +0800

    learning-gem5: add the tutorial progress
    fix the bugs in event-driving programming

--- src/learning_gem5/part2/HelloObject.py
+++ src/learning_gem5/part2/HelloObject.py
@@ -6,5 +6,5 @@ from m5.SimObject import SimObject
 
 class HelloObject(SimObject):
     type = "HelloObject"  # 要封装的C++类
-    cxx_header = "learning_gem5/hello_object.hh"  # 包含C++类的头文件声明
+    cxx_header = "learning_gem5/part2/hello_object.hh"  # 包含C++类的头文件声明
     cxx_class = "gem5::HelloObject"  # 一个属性, 用于指定新创建的SimObject类声明在gem5命名空间中
--- src/learning_gem5/part2/hello_object.cc
+++ src/learning_gem5/part2/hello_object.cc
@@ -4,22 +4,33 @@
 #include <iostream>
 namespace gem5
 {
-HelloObject::HelloObject(const HelloObjectParams &params) : SimObject(params)
+
+HelloObject::HelloObject(const HelloObjectParams &params)
+    : SimObject(params),
+      event([this] { processEvent(); }, name()),
+      latency(100),
+      timesLeft(10)
+{
+    DPRINTF(HelloExample, "Created the hello object\n");
+} // 在构造函数中添加两个变量的默认值
+
+/*HelloObject::HelloObject(const HelloObjectParams &params) :
+    SimObject(params)
 {
     // std::cout << "Hello World! From a SimObject!" << std::endl;
     DPRINTF(HelloExample, "Create the hello object\n");
     // 输出调试信息, 包括DPRINTF执行时的当前时钟周期,
     // 调用DPRINTF的SimObject名称 以及传递的字符串
-}
+}*/
 
-HelloObject::HelloObject(const HelloObjectParams &params)
-    : SimObject(params), event([this] { processEvent(); }, name())
+/*HelloObject::HelloObject(const HelloObjectParams &params) :
+    SimObject(params), event([this]{processEvent();}, name())
 {
     DPRINTF(HelloExample, "Created the hello object\n");
 }
 // 该函数接收两个参数: 1. 一个无参数无返回值的函数 2. 事件的名称,
 // 使用name函数获取 this放入lambda表达式中,
-// 以便在事件触发时调用processEvent函数
+// 以便在事件触发时调用processEvent函数*/
 
 void
 HelloObject::processEvent()
@@ -27,7 +38,7 @@ HelloObject::processEvent()
     timesLeft--;
     DPRINTF(HelloExample, "Hello World! Processing the event!\n");
 
-    if (timesLeft > 0) {
+    if (timesLeft <= 0) {
         DPRINTF(HelloExample, "Done firing!\n");
     } else {
         schedule(event, latency + curTick());
@@ -45,12 +56,4 @@ HelloObject::startup()
 // 安排事件的执行时间, startup()函数在模拟器启动时被调用,
 // 这里安排事件在100周期(可以使用显式时间)后执行
 
-HelloObject::HelloObject(const HelloObjectParams &params)
-    : SimObject(params),
-      event([this] { processEvent(); }, name()),
-      latency(100),
-      timesLeft(10)
-{
-    DPRINTF(HelloExample, "Created the hello object\n");
-} // 在构造函数中添加两个变量的默认值
 } // namespace gem5
